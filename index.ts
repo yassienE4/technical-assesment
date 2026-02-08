61554f2fefd73a614337975eae672272eb7cfa83
yassienE4 <yassienebrahim@gmail.com>
2026-02-09 01:25:22 +0200

    added health endpoint

--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from "express";
+import { prisma } from './lib/prisma';
 
 const app = express();
 const PORT = 8080;
 
 
+app.get("/health", async (req, res) => {
+  const base = {
+    uptime: process.uptime(),
+    timestamp: Date.now()
+  };
+
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+
+    return res.status(200).json({
+      ...base,
+      status: "ok",
+      db: "ok"
+    });
+  } catch {
+    return res.status(503).json({
+      ...base,
+      status: "degraded",
+      db: "down"
+    });
+  }
+});
 
 app.get("/api/home", (req: Request, res: Response) => {
   res.json({ message: "Welcome to the Home API!" });
